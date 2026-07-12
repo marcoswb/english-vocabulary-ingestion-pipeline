6a2dfc0fd92945501714f269b9c3f1d512ec1f0f
Marcos Warmling Berti <marcos_wb@outlook.com>
2026-07-12 09:36:45 -0300

    Update run.py

--- run.py
+++ run.py
@@ -18,8 +18,8 @@ def main():
 if __name__ == "__main__":
 
     logging.basicConfig(
-        filename="pipeline.log",
         level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
     main()
