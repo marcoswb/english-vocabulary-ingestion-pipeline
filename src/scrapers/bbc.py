68d281bae0cd07878bc042b3c99ade3a30c66875
Marcos Warmling Berti <marcos_wb@outlook.com>
2026-08-10 21:27:49 -0300

    refactor scraper BBC

--- src/scrapers/bbc.py
+++ src/scrapers/bbc.py
@@ -7,58 +7,82 @@ class BBC:
     def __init__(self):
         self.__base_url = 'https://www.bbc.com'
         self.__articles_titles = []
+        self.__main_page = BaseScraper(self.__base_url)
+
+    def _get_articles_to_extract(self):
+        self.__main_page.load_page()
+        return self.__main_page.get_itens('a.Anchor-styles__AnchorStyled-sc-651d33db-0')
+
+    def _get_link_from_item(self, item):
+        link = str(item['href'])
+        if self.__main_page.is_link(link):
+            if link.startswith(self.__base_url):
+                check_url = link.replace(self.__base_url, '')
+                if not self.__main_page.is_internal_link(check_url):
+                    return None
+
+            return str(link)
+        elif self.__main_page.is_internal_link(link):
+            return f'{self.__base_url}{link}'
+
+        return None
+
+    @staticmethod
+    def _get_title_article(article_page):
+        div_title = article_page.get_itens('[data-component="headline-block"]')
+        if div_title:
+            return div_title[0].get_text(strip=True)
+
+        return None
+
+    @staticmethod
+    def _get_divs_text(article_page):
+        return article_page.get_itens('[data-component="text-block"] p, [data-component="subheadline-block"] h2')
+
+    @staticmethod
+    def _get_time_published_article(article_page):
+        div_time_published = article_page.get_itens('.Byline-styles__TimeContainerStyled-sc-66f6383-2 hcsYRh')
+        if div_time_published:
+            return format_time(div_time_published[0].get_text(strip=True))
+
+        return None
+
+    @staticmethod
+    def _get_contribuitors_article(article_page):
+        div_contribuitors = article_page.get_itens('.Byline-styles__AuthorNameStyled-sc-66f6383-8')
+        if div_contribuitors:
+            contribuitors = ''
+            for contributor in div_contribuitors:
+                contribuitors += contributor.get_text(strip=True) + ' '
+            return contribuitors
+
+        return None
 
     def extract(self):
-        main_page = BaseScraper(self.__base_url)
-        main_page.load_page()
+        extracted_articles = self._get_articles_to_extract()
+
         articles = []
+        for item in extracted_articles:
+            url = self._get_link_from_item(item)
 
-        for item in main_page.get_itens('a.Anchor-styles__AnchorStyled-sc-651d33db-0'):
-            link = str(item['href'])
-            if main_page.is_link(link):
-                if link.startswith(self.__base_url):
-                    check_url = link.replace(self.__base_url, '')
-                    if not main_page.is_internal_link(check_url):
-                        continue
-
-                url = str(link)
-            elif main_page.is_internal_link(link):
-                url = f'{self.__base_url}{link}'
-            else:
+            if not url or not url.startswith(self.__base_url):
                 continue
-
-            if not url.startswith(self.__base_url):
-                continue
-
-            article = Article('bbc')
-            article.url = url
 
             article_page = BaseScraper(url)
             article_page.load_page()
 
-            div_title = article_page.get_itens('[data-component="headline-block"]')
-            if div_title:
-                article.title = div_title[0].get_text(strip=True)
-            else:
+            article = Article('bbc')
+            article.url = url
+            article.published_at = self._get_time_published_article(article_page)
+            article.author = self._get_contribuitors_article(article_page)
+
+            article.title = self._get_title_article(article_page)
+            if not article.title or article.title in self.__articles_titles:
                 continue
 
-            if article.title in self.__articles_titles:
-                continue
-
-            divs_text = article_page.get_itens('[data-component="text-block"] p, [data-component="subheadline-block"] h2')
+            divs_text = self._get_divs_text(article_page)
             if not divs_text:
                 continue
-
-            div_time_published = article_page.get_itens('.Byline-styles__TimeContainerStyled-sc-66f6383-2 hcsYRh')
-            if div_time_published:
-                article.published_at = format_time(div_time_published[0].get_text(strip=True))
-
-            div_contribuitors = article_page.get_itens('.Byline-styles__AuthorNameStyled-sc-66f6383-8')
-            if div_contribuitors:
-                contribuitors = ''
-                for contributor in div_contribuitors:
-                    contribuitors += contributor.get_text(strip=True) + ' '
-                article.author = contribuitors
 
             for paragraph in divs_text:
                 article.add_text(paragraph)
